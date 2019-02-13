58bdbc07b8a206db6e38e7b03b59aa25309e9c2b
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-02-13 22:35:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -518,6 +518,21 @@ client.on("reachLimit", (limit)=> {
 
 
 
+function codes(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+};
+
+function rebel(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+};
+    client.on('guildMemberAdd',m => {
+    if (rebel(codes(moment(m.user.createdTimestamp).format('l')), codes(moment().format('l'))) < 8) {
+        m.ban();
+    };
+});
+
+
 
         client.login(process.env.BOT_TOKEN);
  
