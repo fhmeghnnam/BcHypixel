c9c815f1bd4509392cd71d316de0d6c19b85b04a
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-02-14 19:37:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -699,6 +699,59 @@ client.on('guildMemberAdd', member => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message',message =>{
+    var prefix = "+";
+    if(message.content.startsWith(prefix + 'topinv')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} `${invs[inv.code]}`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'
+
+**By:** '+message.author}`)
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
+
+
+
+
+
+
+
+
+
         client.login(process.env.BOT_TOKEN);
  
         
