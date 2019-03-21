40d83edbe593379d34741fbb01ca1fafc9ba87a5
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-03-21 17:54:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
 var prefix = "!!";
-
+const Fhme = require("fs");
 
 client.on('message', message => {
 if(message.author.bot) return;
